f439c8710933120c594861b9717c57a658accca6
Douglas Duteil <douglasduteil@gmail.com>
2018-12-26 15:47:06 +0100

    test: use hyperhtml in tests
    Current serializer code is working for hyperhtml 2.4 to 2.18.
    see https://github.com/WebReflection/hyperHTML/issues/300

---
+++ __tests__/actual.js
@@ -0,0 +1,10 @@
+//
+
+exports.FooHyperApp = render =>
+  render`
+  <foo-hyper-app>
+    <!-- This is my comment -->
+    <h1>${"Hello hyperHTML !"}</h1>
+    <h1>${"Hello world !"}</h1>
+  </foo-hyper-app>
+`;
--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,16 +1,15 @@
 //
 
-test('should remove extra hyper comments from the html', () => {
-  const actual = require('fs')
-    .readFileSync(__dirname + '/actual.html')
-    .toString();
-  const section = document.createElement('section');
-  section.innerHTML = actual;
+test("should remove extra hyper comments from the html", () => {
+  const { bind } = require("hyperhtml");
+  const { FooHyperApp } = require("./actual.js");
+  const section = document.createElement("section");
+  FooHyperApp(bind(section));
   expect(section).toMatchSnapshot();
 });
 
-test('should only act on HTML strings', () => {
-  expect('Some text without any tags.').toMatchSnapshot();
+test("should only act on HTML strings", () => {
+  expect("Some text without any tags.").toMatchSnapshot();
   expect({}).toMatchSnapshot();
   expect([]).toMatchSnapshot();
   expect(0).toMatchSnapshot();
